cfe557ce030871fe3393fb0623c5da6c02b8a537
Johnathan Teles <telesjohnathan@gmail.com>
2025-03-18 18:16:05 -0300

    projeto-completo

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 			function contatoSelecionado(event) {
 			    event.preventDefault();
 			    document.querySelector("#contato").scrollIntoView({
@@ -39,3 +40,45 @@
             window.location.href = linkWhatsApp;
         }
 
+=======
+			function contatoSelecionado(event) {
+			    event.preventDefault(); // Evita o comportamento padrão do link
+			    document.querySelector("#contato").scrollIntoView({
+			        behavior: "smooth"  // Rolagem suave
+			    });
+			}
+			function portfolioSelecionado(event) {
+			    event.preventDefault(); // Evita o comportamento padrão do link
+			    document.querySelector("#portfolio").scrollIntoView({
+			        behavior: "smooth"  // Rolagem suave
+			    });
+			}
+			function skillsSelecionado(event) {
+			    event.preventDefault(); // Evita o comportamento padrão do link
+			    document.querySelector("#skills").scrollIntoView({
+			        behavior: "smooth"  // Rolagem suave
+			    });
+			}
+			function sobreSelecionado(event) {
+			    event.preventDefault(); // Evita o comportamento padrão do link
+			    document.querySelector("#sobre").scrollIntoView({
+			        behavior: "smooth"  // Rolagem suave
+			    });
+			}
+			function homeSelecionado(event) {
+			    event.preventDefault(); // Evita o comportamento padrão do link
+			    document.querySelector("#home").scrollIntoView({
+			        behavior: "smooth"  // Rolagem suave
+			    });
+			}
+
+        function redirectToWhatsApp() {
+            let telefone = '5562995552510';  
+			let mensagem = 'Olá, vi seu portfólio e gostaria de saber mais sobre suas habilidades e projetos. Vamos conversar!';
+
+            let linkWhatsApp = `https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`;
+            
+            window.location.href = linkWhatsApp;
+        }
+
+>>>>>>> f6e20bf (projeto-completo)
