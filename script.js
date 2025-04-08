689887433f43fce62c50a9d53713d2acc786bae8
Johnathan Teles <telesjohnathan@gmail.com>
2025-04-08 14:42:56 -0300

    CARROSSEL: JS do carrossel

--- script.js
+++ script.js
@@ -83,3 +83,37 @@
             window.location.href = linkWhatsApp;
         }
 
+
+	//*************CARROSSEL
+	let currentIndex = 0;
+
+	function showSlide(index) {
+	  const slides = document.querySelectorAll('.carousel-images img');
+	  const totalSlides = slides.length;
+
+	  if (index >= totalSlides) {
+	    currentIndex = 0;
+	  } else if (index < 0) {
+	    currentIndex = totalSlides - 1;
+	  } else {
+	    currentIndex = index;
+	  }
+
+	  const slideWidth = document.querySelector('.carousel').offsetWidth;
+	  const offset = -currentIndex * slideWidth;
+	  document.getElementById('carouselImages').style.transform = `translateX(${offset}px)`;
+	}
+
+	function nextSlide() {
+	  showSlide(currentIndex + 1);
+	}
+
+	function prevSlide() {
+	  showSlide(currentIndex - 1);
+	}
+
+	window.onload = () => {
+	  showSlide(currentIndex);
+	};
+	    console.log("Slide atual:", currentIndex);
+	    
